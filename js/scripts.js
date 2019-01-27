0aba2febe4d7db7d6194caef57f542a57ad47913
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-01-26 21:32:42 -0800

    fix bug with 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var symbols = [[1, "I"], [5, "V"], [10, 'X'], [50, "L"], [100, "C"], [500, "D"], [1000, "M"]];
+var symbols = [[1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, 'X'], [40, "XL"], [50, "L"], [90, "XC"], [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"]];
 
 // $(document).ready(function() {
 //
@@ -12,7 +12,7 @@ var symbols = [[1, "I"], [5, "V"], [10, 'X'], [50, "L"], [100, "C"], [500, "D"],
 $(document).ready(function(){
   $("#main").submit(function(event){
     var inputNumber = parseInt($("input").val());
-    var symbolIndex = 6;
+    var symbolIndex = symbols.length - 1;
     var outputText = "";
     var numberOfSameSymbolsInARow = 0;
     var number = inputNumber;
@@ -20,20 +20,30 @@ $(document).ready(function(){
     while(number !== 0){
       if(number-symbols[symbolIndex][0] < 0){
         symbolIndex--;
-        numberOfSameSymbolsInARow = 0;
       } else {
-
+        outputText += symbols[symbolIndex][1];
         number -= symbols[symbolIndex][0];
-        if (numberOfSameSymbolsInARow === 3){
-          console.log(outputText);
-          outputText = outputText.slice(0, outputText.length - 4) + symbols[symbolIndex][1] + symbols[symbolIndex+2][1];
-          numberOfSameSymbolsInARow = 1;
-        } else {
-          outputText += symbols[symbolIndex][1];
-          numberOfSameSymbolsInARow += 1;
-        }
       }
     }
+
+    // while(number !== 0){
+    //   if(number-symbols[symbolIndex][0] < 0){
+    //     symbolIndex--;
+    //     numberOfSameSymbolsInARow = 0;
+    //   } else {
+    //
+    //     number -= symbols[symbolIndex][0];
+    //     if (numberOfSameSymbolsInARow === 3){
+    //       console.log(outputText);
+    //       console.log("slice  " + outputText.slice(0, outputText.length - 4));
+    //       outputText = outputText.slice(0, outputText.length - 4) + symbols[symbolIndex][1] + symbols[symbolIndex+2][1];
+    //       numberOfSameSymbolsInARow = 1;
+    //     } else {
+    //       outputText += symbols[symbolIndex][1];
+    //       numberOfSameSymbolsInARow += 1;
+    //     }
+    //   }
+    // }
 
     $("#result").text(outputText);
 
